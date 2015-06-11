242dcfc18e2a552a1a8b749cf1891db5abb643ba
Tugzrida <codecam@internode.on.net>
2015-06-12 06:23:01 +1000

    Update quote.js

--- wisdomofweek/quote.js
+++ wisdomofweek/quote.js
@@ -1,1 +1,3 @@
-
+var quote = "What lies behind you and what lies in front of you,\npales in comparison to what lies inside of you.";
+var person = "Ralph Waldo Emerson";
+var wiki = "Ralph_Waldo_Emerson";
