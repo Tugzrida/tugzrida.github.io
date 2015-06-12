206da6b9e683adcd5261f5f775c91719e2e7570a
Tugzrida <cameronsteel@Camerons-MacBook-Pro.local>
2015-06-12 18:00:10 +1000

    Update quote.js

--- wisdomofweek/quote.js
+++ wisdomofweek/quote.js
@@ -1,4 +1,5 @@
+var week = "8B";
 var quote = "What lies behind you and what lies in front of you,<br>pales in comparison to what lies inside of you.";
+var quotelines = "2";
 var person = "Ralph Waldo Emerson";
 var wiki = "Ralph_Waldo_Emerson";
-var week = "8B";
