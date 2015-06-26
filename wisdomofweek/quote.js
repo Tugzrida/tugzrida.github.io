87b263c1fa0cd79db5b4fb3762b7f5e4ae73caae
Tugzrida <codecam@internode.on.net>
2015-06-26 21:01:43 +1000

    Update quote.js

--- wisdomofweek/quote.js
+++ wisdomofweek/quote.js
@@ -1,5 +1,5 @@
 var week = "10B";
-var quote = "Without courage, wisdom bears no fruit";
+var quote = "Without courage, wisdom bears no fruit.";
 var quotelines = 1;
 var person = "Baltasar Gracián";
 var wiki = "Baltasar_Gracián";
