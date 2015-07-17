993d6424c2bbb18e929a8a97ba12a9bf256d49d4
Tugzrida <cameronsteel@Camerons-MacBook-Pro.local>
2015-07-17 21:38:53 +1000

    Update quote.js

--- wisdomofweek/quote.js
+++ wisdomofweek/quote.js
@@ -1,5 +1,5 @@
 var week = "1A";
-var quote = "The difference between being ordinary and<br>being extraordinary, is that little bit extra";
+var quote = "The difference between being ordinary and<br>being extraordinary, is that little bit extra.";
 var quotelines = 2;
 var person = "Jimmy Johnson";
 var wiki = "Jimmy_Johnson_(American_football_coach)";
