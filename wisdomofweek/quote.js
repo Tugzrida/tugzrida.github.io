96fb8aa9995dcdf623317ede3a1602272b84f945
Tugzrida <codecam@internode.on.net>
2015-07-17 18:20:45 +1000

    Update quote.js

--- wisdomofweek/quote.js
+++ wisdomofweek/quote.js
@@ -1,5 +1,5 @@
-var week = "10B";
-var quote = "Without courage, wisdom bears no fruit.";
-var quotelines = 1;
-var person = "Baltasar Gracián";
-var wiki = "Baltasar_Gracián";
+var week = "1A";
+var quote = "The difference between being ordinary and<br>being extraordinary, is that little bit extra";
+var quotelines = 2;
+var person = "Jimmy Johnson";
+var wiki = "Jimmy_Johnson_(American_football_coach)";
