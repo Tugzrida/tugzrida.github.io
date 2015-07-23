3662ad45388262029785fa997a27cefe90b96750
Tugzrida <codecam@internode.on.net>
2015-07-23 16:22:23 +1000

    Update quote.js

--- wisdomofweek/quote.js
+++ wisdomofweek/quote.js
@@ -1,5 +1,5 @@
 var week = "2B";
-var quote = "You never really know someone until<br>you have walked in their shoes.";
+var quote = "You never really know someone until you have<br>walked in their shoes.";
 var quotelines = 2;
 var person = "To Kill a Mockingbird by Harper Lee";
 var wiki = "To_Kill_a_Mockingbird";
