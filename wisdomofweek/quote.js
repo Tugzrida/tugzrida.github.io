177c92b614b95af1afadbe7adecab83ab8c9c1db
Tugzrida <cameronsteel@Camerons-MacBook-Pro.local>
2015-08-08 12:28:42 +1000

    Update quote.js

--- wisdomofweek/quote.js
+++ wisdomofweek/quote.js
@@ -1,5 +1,5 @@
 var week = "5A";
-var quote = "Teachers open the door. You must enter by yourself";
+var quote = "Teachers open the door. You must enter by yourself.";
 var quotelines = 1;
 var person = "Chinese Proverb";
 var wiki = "Proverbs_commonly_attributed_to_be_Chinese";
