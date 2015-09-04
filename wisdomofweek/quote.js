b4d58c7393108c5300976da98aed704342476629
Tugzrida <cameronsteel@Camerons-MacBook-Pro.local>
2015-09-04 15:03:16 +1000

    Update quote.js

--- wisdomofweek/quote.js
+++ wisdomofweek/quote.js
@@ -1,5 +1,5 @@
-var week = "8B";
-var quote = "You don’t have to be great to start, but you do have to<br>start to be great.";
-var quotelines = 2;
-var person = "Zig Ziglar";
-var wiki = "Zig_Ziglar";
+var week = "9A";
+var quote = "Life is a journey, not a destination.";
+var quotelines = 1;
+var person = "Ralph Waldo Emerson";
+var wiki = "Ralph_Waldo_Emerson";
