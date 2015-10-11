db4b0b1d0d49e0279bdc4754ac19a1d304da8125
Tugzrida <codecam@internode.on.net>
2015-10-12 08:11:15 +1100

    Update quote.js

--- wisdomofweek/quote.js
+++ wisdomofweek/quote.js
@@ -1,5 +1,5 @@
 var week = "2B";
-var quote = "The secret of getting ahead is getting started";
+var quote = "The secret of getting ahead is getting started.";
 var quotelines = 1;
 var person = "Mark Twain";
 var wiki = "Mark_Twain";
