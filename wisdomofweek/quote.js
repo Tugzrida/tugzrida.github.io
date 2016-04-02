c2aec41fdc1bfbcb4c087405c9fb42a933e61e3f
Tugzrida <tugzrida@gmail.com>
2016-04-02 11:30:54 +1100

    Update quote.js

--- wisdomofweek/quote.js
+++ wisdomofweek/quote.js
@@ -1,5 +1,5 @@
 var week = "11A";
 var quote = "Our greatest weakness lies in giving up. The most certain<br>way to succeed is always to try just one more time.";
-var quotelines = 2;
+var quotelines = 3;
 var person = "Thomas Edison";
 var wiki = "Thomas_Edison";
