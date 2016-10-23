87de458352739e2cc7d2aee2ba5fef7d35ee361f
Tugzrida <tugzrida@gmail.com>
2016-10-23 21:51:51 +1100

    General updates and tidy up

--- wisdomofweek/quote.js
+++ wisdomofweek/quote.js
@@ -1,4 +1,3 @@
-var week = "10A";
 var quote = "And you don't have to change a thing, the world could<br>change its heart";
 var quotelines = 2;
 var person = "'Scars To Your Beautiful' by Alessia Cara";
