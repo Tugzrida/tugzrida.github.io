cec05d2591f0e829f14fa7daa262d96be7ccc089
Tugzrida <tugzrida@gmail.com>
2017-01-30 18:45:26 +1100

    Update quote.js

--- wisdomofweek/quote.js
+++ wisdomofweek/quote.js
@@ -1,4 +1,4 @@
-var quote = "And you don't have to change a thing, the world could<br>change its heart";
-var quotelines = 2;
-var person = "'Scars To Your Beautiful' by Alessia Cara";
-var wiki = "Scars_to_Your_Beautiful";
+var quote = "It is two and a half minutes to midnight";
+var quotelines = 1;
+var person = "Doomsday Clock";
+var wiki = "Doomsday_Clock";
