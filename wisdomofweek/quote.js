80d5420fbd1a243f099b34bf7486fbbf461da536
Cameron Steel <Tugzrida@users.noreply.github.com>
2018-01-25 13:52:48 +1100

    Update quote.js

--- wisdomofweek/quote.js
+++ wisdomofweek/quote.js
@@ -1,4 +1,4 @@
-var quote = '<span style="color:#f00">#</span><span style="color:#f50">L</span><span style="color:#ff0">o</span><span style="color:#5f0">v</span><span style="color:#0f0">e</span><span style="color:#0f5">W</span><span style="color:#0ff">i</span><span style="color:#05f">n</span><span style="color:#00f">s</span>';
-var quotelines = 1;
-var person = "Australia passes marriage equality";
-var wiki = "History_of_same-sex_marriage_in_Australia";
+var quote = 'Music gives a soul to the universe, wings to the mind, flight<br>to the imagination and life to everything.';
+var quotelines = 2;
+var person = "Plato";
+var wiki = "Plato";
